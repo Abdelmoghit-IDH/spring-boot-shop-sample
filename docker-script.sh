38edbb6fc55e53f6f7ba14c43d09d48696346c78
abdelmoghit idhsaine <idhsaine.abdelmoghit@inemail.ine.inpt.ma>
2022-03-19 17:58:17 +0100

    add script docker

--- docker-script.sh
+++ docker-script.sh
@@ -4,4 +4,6 @@ echo "Stop old container and run a new one"
 
 docker container stop $(docker ps -qa) 
 docker container rm $(docker ps -qa) 
+docker image rm $(docker image ls -qa)
+
 docker run -d -p 80:8080 --name $1 $2
